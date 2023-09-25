70ed14da5b16d1c77c54ac283370f0e356968560
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-25 20:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	 if (sampleObject.hasOwnProperty(key)) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+// Define the global object sampleObject
+const sampleObject = {
+    red: "#FF0000",
+    green: "#00FF00",
+    white: "#FFFFFF"
+};
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
